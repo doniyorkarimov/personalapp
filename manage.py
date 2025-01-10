fa4cc29c14c399cf10482ef01238984cc3979872
Doniyor <doniyor23323a@gmail.com>
2025-01-10 10:31:26 +0500

    tahrirlandi1

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
@@ -21,7 +21,6 @@ def main():
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogproject.settings")
     port = os.environ.get("PORT", "8000") 
-    from django.core.management import execute_from_command_line
     execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
 
 
