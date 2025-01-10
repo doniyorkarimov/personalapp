94e52ba68b33818874aad74e0837fc20044fc91c
Doniyor <doniyor23323a@gmail.com>
2025-01-10 10:19:06 +0500

    tahrirlandi

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ def main():
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogproject.settings")
     port = os.environ.get("PORT", "8000") 
     from django.core.management import execute_from_command_line
     execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
