b71cf6b7b5dc1d2072914b7b6a0bc39b8e21e38b
Doniyor <doniyor23323a@gmail.com>
2025-01-10 18:22:10 +0500

    edit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -129,6 +129,8 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+
 AUTH_USER_MODEL = 'userapp.UserProfile'
 
 # Default primary key field type
