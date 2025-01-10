ad772d2d94f5828099f54d31381b8a841d2213fd
Doniyor <doniyor23323a@gmail.com>
2025-01-10 10:08:52 +0500

    port qoshildi

--- manage.py
+++ manage.py
@@ -18,5 +18,12 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+    port = os.environ.get("PORT", "8000") 
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
+
+
 if __name__ == '__main__':
     main()
